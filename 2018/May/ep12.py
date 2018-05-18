cb272915af081f8271b6dcad87a27ca824dabfd7
Casey Mau <mauza@mauza.net>
2018-05-17 21:08:03 -0600

    another euler problem

---
+++ 2018/May/ep12.py
@@ -0,0 +1,43 @@
+
+def divisors(n):
+    # get factors and their counts
+    factors = {}
+    nn = n
+    i = 2
+    while i*i <= nn:
+        while nn % i == 0:
+            if not i in factors:
+                factors[i] = 0
+            factors[i] += 1
+            nn //= i
+        i += 1
+    if nn > 1:
+        factors[nn] = 1
+
+    primes = list(factors.keys())
+
+    # generates factors from primes[k:] subset
+    def generate(k):
+        if k == len(primes):
+            yield 1
+        else:
+            rest = generate(k+1)
+            prime = primes[k]
+            for factor in rest:
+                prime_to_i = 1
+                # prime_to_i iterates prime**i values, i being all possible exponents
+                for _ in range(factors[prime] + 1):
+                    yield factor * prime_to_i
+                    prime_to_i *= prime
+
+    yield from generate(0)
+
+def sum_num(n):
+    return sum(range(1, n+1))
+
+for i in range(1, 1000000):
+    tri_num = sum_num(i)
+    if len(list(divisors(tri_num))) >= 500:
+        print(i)
+        print(tri_num)
+        break
