f72e792279ed696eac07101e5040db802b0d40b9
Casey Mau <mauza@mauza.net>
2018-05-20 19:37:08 -0600

    another few euler problems

--- 2018/May/ep26.py
+++ 2018/May/ep26.py
@@ -1,7 +1,18 @@
-from decimal import *
-getcontext().prec = 28
 
-one = Decimal('1.0')
-for i in range(2,1000):
-    some = one/Decimal(i)
-    
+def find_repeat(n):
+    for x in range(1,1000):
+        nines = '9'*x
+        if int(nines)%n == 0:
+            return len(nines)
+    return 1
+
+num = 1
+max_repeat = 0
+for i in range(1,1000):
+    tmp_max = find_repeat(i)
+    if tmp_max > max_repeat:
+        max_repeat = tmp_max
+        num = i
+
+print(num)
+print(max_repeat)
