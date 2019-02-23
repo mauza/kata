18b1d0dba8b37cd54df7ad4ae670dc4303e07738
Casey Mau <mauza@mauza.net>
2019-02-22 21:14:47 -0700

    there was an attempt

--- 2019/Feb/kaplan/get_questions.py
+++ 2019/Feb/kaplan/get_questions.py
@@ -1,4 +1,5 @@
 import asyncio
+
 from pyppeteer import launch
 from bs4 import BeautifulSoup
 from re import sub
@@ -6,13 +7,22 @@ from re import sub
 import requests
 
 async def get_html(url):
-    browser = await launch({'headless': True, 'executablePath': '/usr/bin/chromium'}, args=['--no-sandbox'])
+    browser = await launch( 
+        {'headless': False, 
+        'executablePath': '/opt/google/chrome/chrome',
+        'args': ['--user-data-dir=/home/mauza/.config/google-chrome']}
+    )
     page = await browser.newPage()
-    await page.goto(url)
-    html = await page.content()
+    await page.goto(url, waitUntil='domcontentloaded')
+    await page.waitFor('[data-action="getSummary"]', timeout=7000)
+    pages = await page.querySelectorAll('[data-action="getSummary"]')
+    #print(await pages[0].querySelectorEval('[data-action="getSummary"]', 'node => node.innerText'))
+    await pages[0].click()
+    await page.waitFor(7000)
+    #await asyncio.gather(pages[0].click(), page.waitForNavigation(),)
     await browser.close()
-    return html
+    return pages
 
-url = 'https://www.kaplanlearn.com/education/test/getQuestion/23496054?testId=91015283&position=1'
-html = asyncio.get_event_loop().run_until_complete(get_html(url))
-print(html)
+
+url = 'https://www.kaplanlearn.com/offline/qbank/23496054'
+asyncio.get_event_loop().run_until_complete(get_html(url))
