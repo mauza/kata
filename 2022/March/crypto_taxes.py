cba0bfa31e30a78d2b2ba714c909c37c782e95b5
mauza <mauza@mauza.net>
2022-03-23 16:11:53 -0600

    added pandas and calculated the cost at the time

--- 2022/March/crypto_taxes.py
+++ 2022/March/crypto_taxes.py
@@ -1,12 +1,15 @@
+from dateutil import parser
 from datetime import datetime
 from dotenv import load_dotenv
 import os, requests, sys, json
+import pandas
 
 load_dotenv()
 
-SYMBOL = "ETH"
 API_KEY = os.environ["CRYPTOCOMPARE_API_KEY"]
-
+BTC = pandas.read_json('data/BTC_2021.json', lines=True)
+ETH = pandas.read_json('data/ETH_2021.json', lines=True)
+prices = {"BTC": BTC, "ETH": ETH}
 
 def get_data(start_ts, end_ts):
     data = []
@@ -20,14 +23,33 @@ def get_data(start_ts, end_ts):
         data += response["Data"]["Data"]
     return data
 
-def main():
-    filename = f'data/{SYMBOL}_2021.json'
-    data = get_data(datetime.timestamp(datetime(2021, 1, 1)), datetime.timestamp(datetime(2021, 12, 31)))
+def get_historical(year, symbol):
+    filename = f'data/{symbol}_{year}.json'
+    data = get_data(datetime.timestamp(datetime(year, 1, 1)), datetime.timestamp(datetime(year, 12, 31)))
     for hour_data in data:
         d = json.dumps(hour_data)
         with open(filename, 'a') as f:
             f.write(d + "\n")
+
+def find_neighbours(value, df, colname):
+    exactmatch = df[df[colname] == value]
+    if not exactmatch.empty:
+        return exactmatch.index
+    else:
+        lowerneighbour_ind = df[df[colname] < value][colname].idxmax()
+        upperneighbour_ind = df[df[colname] > value][colname].idxmin()
+        return [lowerneighbour_ind, upperneighbour_ind]  
+
+def get_price(t, symbol):
+    ts = datetime.timestamp(t)
+    ns = find_neighbours(ts, prices[symbol], "time")
+    result = prices[symbol].at[ns[0], "low"]
+    return result
  
+def main():
+    account = pandas.read_csv('data/account.csv')
+    account["Current_USD_Conversion"] = account["time"].map(lambda t: get_price(parser.parse(t), "BTC"))
+    account.to_csv('data/test.csv', index=False)
 
 if __name__ == "__main__":
     main()
